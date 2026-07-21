961b0f96b4a9650fecef5e19b5a56af5ded441de
reinaq996-os <qosereina6@gmail.com>
2026-07-21 14:44:11 +0200

    render page with a Jinja template

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello, Tirana!"
+    return render_template("index.html", city="Tirana")
 
 
 if __name__ == "__main__":
